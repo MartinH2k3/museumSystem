11509fc91ce713dcc47ee5499da2d4f3e48e13d5
MartinH2k3 <martinko.horsky.2003@gmail.com>
2024-04-22 02:25:54 +0200

    update

--- purely_sql_version_of_functions/begin_inspection.sql
+++ purely_sql_version_of_functions/begin_inspection.sql
@@ -1,4 +1,2 @@
- --   query = f"INSERT INTO Inspections (artifact_id, start_date, end_date) VALUES ((SELECT id FROM Artifacts WHERE LOWER(name) = LOWER('{artifact_name}')), CURRENT_DATE, CURRENT_DATE + (SELECT inspection_duration FROM Artifacts WHERE LOWER(name) = LOWER('{artifact_name}')));"
---    query = f"UPDATE Artifacts SET status = 'being inspected' WHERE LOWER(name) = LOWER('{artifact_name}');"
 INSERT INTO Inspections (artifact_id, start_date, end_date) VALUES ((SELECT id FROM Artifacts WHERE LOWER(name) = LOWER(:artifact_name)), CURRENT_DATE, CURRENT_DATE + (SELECT inspection_duration FROM Artifacts WHERE LOWER(name) = LOWER(:artifact_name)));
 UPDATE Artifacts SET status = 'being inspected' WHERE LOWER(name) = LOWER(:artifact_name);
