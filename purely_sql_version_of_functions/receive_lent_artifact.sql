bba729414c66c1b5a9670d56296c7119cc1f8a54
MartinH2k3 <martinko.horsky.2003@gmail.com>
2024-04-22 02:58:40 +0200

    update

--- purely_sql_version_of_functions/receive_lent_artifact.sql
+++ purely_sql_version_of_functions/receive_lent_artifact.sql
@@ -5,6 +5,8 @@ INSERT INTO Inspections (artifact_id, start_date, end_date) VALUES
     CURRENT_DATE + (SELECT inspection_duration FROM Artifacts WHERE LOWER(name) = LOWER(:artifact_name))
 );
 
+UPDATE Artifacts SET status = 'being inspected' WHERE LOWER(name) = LOWER(:artifact_name);
+
 UPDATE Loans SET end_date = CURRENT_DATE
 WHERE artifact_id =
     (SELECT id FROM Artifacts WHERE LOWER(name) = LOWER(:artifact_name)) AND end_date IS NULL;
